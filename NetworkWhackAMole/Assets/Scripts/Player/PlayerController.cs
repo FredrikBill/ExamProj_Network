12cb128a51df00c081451c8c40928f296705cc53
FredrikBill <fredrikbillskola@gmail.com>
2018-11-21 15:03:00 +0100

    Revert "Changes to the hurt flashes"
    This reverts commit c2a8fd4e9dea8c7267fe83c999ed449984685822.

--- NetworkWhackAMole/Assets/Scripts/Player/PlayerController.cs
+++ NetworkWhackAMole/Assets/Scripts/Player/PlayerController.cs
@@ -9,18 +9,7 @@ public class PlayerController : PlayerBase {
 	//Also currently takes care of animations
 
 	[SerializeField]
-	private AnimationCurve hurtCurve;
-
-	[SerializeField]
-	private Color hurtColor;
-
-	private float invincibilityTime
-	{
-		get
-		{
-			return hurtCurve.keys[hurtCurve.keys.Length - 1].time;
-		}
-	}
+	private float invincibilityTime = 2f;
 	private float timer;
 
 	private Hammer myHammer;
@@ -90,12 +79,7 @@ public class PlayerController : PlayerBase {
 	//Not tested at all, going to be used as visual cue for taking damage
 	private IEnumerator InvincibilityFlicker()
 	{
-		MeshRenderer[] meshRends = GetComponentsInChildren<MeshRenderer>();
-		Material[] bodyMaterials = new Material[meshRends.Length];
-		for (int i = 0; i < meshRends.Length; i++)
-		{
-			bodyMaterials[i] = meshRends[i].material;
-		}
+		Material[] bodyMaterials = GetComponentsInChildren<Material>();
 		Color[] startColors = new Color[bodyMaterials.Length];
 		Color[] targetColors = new Color[bodyMaterials.Length];
 		for (int i = 0; i < bodyMaterials.Length; i++)
@@ -105,14 +89,16 @@ public class PlayerController : PlayerBase {
 			targetColors[i].a = 0;
 		}
 		float timer  = 0;
+		float inTime = invincibilityTime;
+		float currentValue;
 
 		while(timer <= invincibilityTime)
 		{
-			timer += Time.deltaTime;
-
+			currentValue = timer / inTime;
+			currentValue = Mathf.Sin(currentValue * Mathf.PI * .5f);
 			for (int i = 0; i < bodyMaterials.Length; i++)
 			{
-				bodyMaterials[i].color = Color.Lerp(startColors[i], hurtColor, hurtCurve.Evaluate(timer));
+				bodyMaterials[i].color = Color.Lerp(startColors[i], targetColors[i], currentValue);
 			}
 			yield return new WaitForEndOfFrame();
 		}
